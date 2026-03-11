a402258c5c891d7bf591bd41ac0f69c849734567
shubhammaurya1204 <shubhammaurya1204@gmail.com>
2026-03-11 15:39:49 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 // CORS
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://treebay-assignment-client.vercel.app/",
     credentials: true,
   })
 );
